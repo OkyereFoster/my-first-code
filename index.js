e1917447c3a4714fc8dfc91ba3215da80d907406
OkyereFoster <okyerefoster1204@gmail.com>
2026-02-28 14:19:31 +0000

    add guessing game

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let secret = 5;
 let guess = 3;
-
 if (secret === guess) {
   console.log("correct");
+} else if (secret > guess) {
+  console.log("too low");
 } else {
-  console.log("try again");
+  ("too high");
 }
